be900c82cf19f2ff69d15969ba58c72a13815f3a
dvargas42 <dvargas.eng@gmail.com>
2025-02-21 16:42:04 -0300

    feat: :sparkles: create EventConflictException

---
+++ src/main/java/br/com/nlw/events/exception/EventConflictException.java
@@ -0,0 +1,7 @@
+package br.com.nlw.events.exception;
+
+public class EventConflictException extends RuntimeException {
+    public EventConflictException(String message) {
+        super(message);
+    }
+}
