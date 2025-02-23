e584cc983ce9270f6eda808069f28bcdf4f1afc5
dvargas42 <dvargas.eng@gmail.com>
2025-02-23 03:10:28 -0300

    feat: :speech_balloon: include  single quotes in EventIn

--- src/main/java/br/com/nlw/events/dto/EventIn.java
+++ src/main/java/br/com/nlw/events/dto/EventIn.java
@@ -10,28 +10,28 @@ import java.time.LocalTime;
 
 public record EventIn(
 
-    @NotHtml(message = "The field title must not have HTML tags")
-    @NotSql(message = "The field title must not have SQL")
-    @NotBlank(message = "The field title must not be null or blank")
+    @NotHtml(message = "The field 'title' must not have HTML tags")
+    @NotSql(message = "The field 'title' must not have SQL")
+    @NotBlank(message = "The field 'title' must not be null or blank")
     String title,
 
-    @NotHtml(message = "The field location must not have HTML tags")
-    @NotSql(message = "The field location must not have SQL")
-    @NotBlank(message = "The field location must not be null or blank")
+    @NotHtml(message = "The field 'location' must not have HTML tags")
+    @NotSql(message = "The field 'location' must not have SQL")
+    @NotBlank(message = "The field 'location' must not be null or blank")
     String location,
 
-    @Price(message = "The field price must be positive")
-    @NotNull(message = "The field price must not be null")
+    @Price(message = "The field 'price' must be positive")
+    @NotNull(message = "The field 'price' must not be null")
     Double price,
 
-    @Future(message = "The field startDate must be in the Future")
-    @Date(message = "The field startDate must have format yy-mm-dd")
-    @NotNull(message = "The field starDate must not be null")
+    @Future(message = "The field 'startDate' must be in the Future")
+    @Date(message = "The field 'startDate' must have format yyyy-mm-dd")
+    @NotNull(message = "The field 'starDate' must not be null")
     LocalDate startDate,
 
-     @Future(message = "The field endDate must not be in Past")
-     @Date(message = "The field endDate must have format yyyy-MM-dd")
-     @NotNull(message = "The field endDate must not be null")
+     @Future(message = "The field 'endDate' must not be in Past")
+     @Date(message = "The field 'endDate' must have format yyyy-MM-dd")
+     @NotNull(message = "The field 'endDate' must not be null")
      LocalDate endDate,
 
     @Time(message = "The field 'startTime' must have format hh:mm:ss")
