872d56d6641bd1b917252641f98878a267f7b9a7
dvargas42 <dvargas.eng@gmail.com>
2025-02-23 03:05:00 -0300

    feat: :sparkles: add EnableCaching annotation

--- src/main/java/br/com/nlw/events/EventsApplication.java
+++ src/main/java/br/com/nlw/events/EventsApplication.java
@@ -2,8 +2,10 @@ package br.com.nlw.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EventsApplication {
 
 	public static void main(String[] args) {
