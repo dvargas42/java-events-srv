aad964bcd908be46cf6db21c7fd27778b9d9967d
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 14:19:47 -0300

    feat: :sparkles: create AISearchInvalidAPIKeyException

---
+++ src/main/java/br/com/nlw/events/exception/AISearchInvalidAPIKeyException.java
@@ -0,0 +1,7 @@
+package br.com.nlw.events.exception;
+
+public class AISearchInvalidAPIKeyException extends Exception {
+    public AISearchInvalidAPIKeyException(String message) {
+        super(message);
+    }
+}
