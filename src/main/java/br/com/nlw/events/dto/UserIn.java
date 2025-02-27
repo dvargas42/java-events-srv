c2feeffae1ce30c1e5fcc9d35960f716db7728f7
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 08:12:02 -0300

    feat: :sparkles: create UserIn record

---
+++ src/main/java/br/com/nlw/events/dto/UserIn.java
@@ -0,0 +1,18 @@
+package br.com.nlw.events.dto;
+
+import br.com.nlw.events.validation.Email;
+import br.com.nlw.events.validation.NotHtml;
+import br.com.nlw.events.validation.NotSql;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserIn(
+
+    @NotSql(message = "The field 'name' must not have SQL")
+    @NotHtml(message = "The field 'name' must not have HTML tags")
+    @NotBlank(message = "The field 'name' must not be null or blank")
+    String name,
+
+    @Email(message = "The field 'email' must be valid")
+    @NotBlank(message = "The field 'email' must not be null or blank")
+    String email
+) {}
