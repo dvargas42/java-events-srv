8bb3e2c6c96755197841a18cc808731bfd4b392d
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 09:39:41 -0300

    feat: :sparkles: change of Use entity to UseIn DTO

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -1,19 +1,25 @@
 package br.com.nlw.events.controller;
 
-import br.com.nlw.events.dto.ErrorMessage;
-import br.com.nlw.events.dto.SubscriptionOut;
-import br.com.nlw.events.dto.SubscriptionRankingItem;
-import br.com.nlw.events.exception.EventNotFoundException;
-import br.com.nlw.events.exception.SubscriptionConflictException;
-import br.com.nlw.events.exception.UserIndicatorNotFoundException;
-import br.com.nlw.events.model.User;
-import br.com.nlw.events.service.SubscriptionService;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.nlw.events.dto.ErrorMessage;
+import br.com.nlw.events.dto.SubscriptionOut;
+import br.com.nlw.events.dto.SubscriptionRankingItem;
+import br.com.nlw.events.dto.UserIn;
+import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.exception.SubscriptionConflictException;
+import br.com.nlw.events.exception.UserIndicatorNotFoundException;
+import br.com.nlw.events.service.SubscriptionService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/subscription")
@@ -22,14 +28,14 @@ public class SubscriptionController implements ISubscriptionController {
     @Autowired
     private SubscriptionService service;
 
-    @PostMapping({"/{prettyName}", "/{prettyName}/{userId}"})
+    @PostMapping({ "/{prettyName}", "/{prettyName}/{userId}" })
     public ResponseEntity<?> createSubscription(
             @PathVariable String prettyName,
             @PathVariable(required = false) Integer userId,
-            @RequestBody User subscriber
+            @Valid @RequestBody UserIn subscriber
     ) {
         try {
-            SubscriptionOut res = service.createNewSubscription(prettyName, subscriber, userId);
+            SubscriptionOut res = service.createSubscription(prettyName, userId, subscriber);
             if (res != null) {
                 return ResponseEntity.ok(res);
             }
@@ -48,7 +54,7 @@ public class SubscriptionController implements ISubscriptionController {
             if (completeRanking.size() < 3) {
                 return ResponseEntity.ok(service.getCompleteRanking(prettyName));
             }
-            return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0,3));
+            return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0, 3));
         } catch (EventNotFoundException ex) {
             return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
@@ -58,7 +64,7 @@ public class SubscriptionController implements ISubscriptionController {
     public ResponseEntity<?> generateRankingByUserId(@PathVariable String prettyName, @PathVariable Integer userId) {
         try {
             return ResponseEntity.ok(service.getRankingByUser(prettyName, userId));
-        } catch (Exception ex) {
+        } catch (UserIndicatorNotFoundException ex) {
             return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
     }
