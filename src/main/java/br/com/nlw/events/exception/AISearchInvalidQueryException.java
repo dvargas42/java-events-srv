36f16a441b09e5d6c7460e14a81f01db422eef97
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 13:53:39 -0300

    feat: :sparkles: create AISearchInvalidQueryException

---
+++ src/main/java/br/com/nlw/events/exception/AISearchInvalidQueryException.java
@@ -0,0 +1,8 @@
+package br.com.nlw.events.exception;
+
+public class AISearchInvalidQueryException extends RuntimeException {
+    public AISearchInvalidQueryException(String message) {
+        super(message);
+    }
+    
+}
