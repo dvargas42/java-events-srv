24d568dd70656195dc7f787afcf4c56dba4c6890
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 13:52:14 -0300

    feat: :sparkles: create AISearchMarkDownOut

---
+++ src/main/java/br/com/nlw/events/dto/AISearchMarkDownOut.java
@@ -0,0 +1,10 @@
+package br.com.nlw.events.dto;
+
+import java.math.BigDecimal;
+
+public record AISearchMarkDownOut(
+    String result,
+    Integer imputCountTokens,
+    Integer outputCountTokens,
+    BigDecimal costEstimate
+) { }
