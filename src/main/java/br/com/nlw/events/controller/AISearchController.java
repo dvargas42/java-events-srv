bd26f2e9ca5f685f6aa988c346975d12eff7bccd
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 13:50:52 -0300

    feat: :sparkles: create AISearchController

---
+++ src/main/java/br/com/nlw/events/controller/AISearchController.java
@@ -0,0 +1,31 @@
+package br.com.nlw.events.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.nlw.events.dto.AISearchIn;
+import br.com.nlw.events.service.AISearchService;
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("/ai-search")
+public class AISearchController {
+    
+    private final AISearchService aiService;
+
+    public AISearchController(AISearchService aiService) {
+        this.aiService = aiService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> generateResponse(@Valid @RequestBody AISearchIn aiSearchIn) throws Exception {
+        if (aiSearchIn.formatAsMarkdown()) {
+            return ResponseEntity.ok().body(aiService.generateMarkDownResponse(aiSearchIn.prompt()));
+        }
+        return ResponseEntity.ok().body(aiService.generateJsonResponse(aiSearchIn.prompt()));
+    }
+}
