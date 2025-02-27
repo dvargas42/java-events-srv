98b689b6ebd5ca133acd9ea65fb3c4d2f52ec848
dvargas42 <dvargas.eng@gmail.com>
2025-02-27 14:20:37 -0300

    feat: :sparkles: create AISearchSendCharException

---
+++ src/main/java/br/com/nlw/events/exception/AISearchSendChatException.java
@@ -0,0 +1,8 @@
+package br.com.nlw.events.exception;
+
+public class AISearchSendChatException extends Exception {
+    
+    public AISearchSendChatException(String message) {
+        super(message);
+    } 
+}
