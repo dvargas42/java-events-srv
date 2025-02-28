803e7d512fbf64e8895383ae25fffe1a37c95483
dvargas42 <dvargas.eng@gmail.com>
2025-02-28 14:25:44 -0300

    feat: :sparkles: create AsyncConfig

---
+++ src/main/java/br/com/nlw/events/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package br.com.nlw.events.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+        return executor;
+    }
+}
