b0642883c9da0e564ca1604414b20a7dd9ed2a2e
dvargas42 <dvargas.eng@gmail.com>
2025-02-28 14:26:45 -0300

    feat: :sparkles: create EmailSubscriptionCompleted

---
+++ src/main/java/br/com/nlw/events/email/EmailSubscrionCompleted.java
@@ -0,0 +1,24 @@
+package br.com.nlw.events.email;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailSubscrionCompleted {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmailSubscrionCompleted.class);
+    
+    private final EmailService emailService;
+
+    public EmailSubscrionCompleted(EmailService emailservice) {
+        this.emailService = emailservice;
+    }
+
+    @Async("asyncExecutor")
+    public void execute(String to, String subject, String content){
+        emailService.sendEmail(to, subject, content);
+        logger.info("Email thread " + Thread.currentThread().getName());
+    }
+}
