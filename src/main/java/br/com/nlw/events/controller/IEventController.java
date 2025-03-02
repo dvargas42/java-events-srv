3d46ecef4b3de4afc7eb50d8210fcdc1bf30971c
dvargas42 <dvargas.eng@gmail.com>
2025-03-02 14:57:04 -0300

    feat: :fire: remove ApiResponses annotations

--- src/main/java/br/com/nlw/events/controller/IEventController.java
+++ src/main/java/br/com/nlw/events/controller/IEventController.java
@@ -9,35 +9,28 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Event", description = "Event information")
 public interface IEventController {
 
     @Operation(summary = "Event register", description = "This functionality is responsible for creating a event")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", content = {
-            @Content(schema = @Schema(implementation = Event.class))
-        })
+    @ApiResponse(responseCode = "200", content = {
+        @Content(schema = @Schema(implementation = Event.class))
     })
     ResponseEntity<?> addNewEvent(EventIn eventIn, UriComponentsBuilder uriBuilder);
 
     @Operation(summary = "Event search all", description = "This functionality is responsible for search all events")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", content = {
-            @Content(schema = @Schema(implementation = Event.class))
-        }),
-        @ApiResponse(responseCode = "404", description = "Event not found")
+    @ApiResponse(responseCode = "200", content = {
+        @Content(schema = @Schema(implementation = Event.class))
     })
+    @ApiResponse(responseCode = "404", description = "Event not found")
     ResponseEntity<?> getAllEvents();
 
     @Operation(summary = "Event search by prettyName", description = "This functionality is responsible for search a event by prettyName")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", content = {
-            @Content(schema = @Schema(implementation = Event.class))
-        }),
-        @ApiResponse(responseCode = "404", description = "Event not found")
+    @ApiResponse(responseCode = "200", content = {
+        @Content(schema = @Schema(implementation = Event.class))
     })
+    @ApiResponse(responseCode = "404", description = "Event not found")
     ResponseEntity<?> getByPrettyName(String id);
 }
