08c798f3210e74455ea33b4a7a99f8278ec3be90
dvargas42 <dvargas.eng@gmail.com>
2025-03-02 14:43:07 -0300

    feat: :zap: add EnableAsync annotation

--- src/main/java/br/com/nlw/events/EventsApplication.java
+++ src/main/java/br/com/nlw/events/EventsApplication.java
@@ -3,9 +3,11 @@ package br.com.nlw.events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@EnableAsync
 @EnableCaching
+@SpringBootApplication
 public class EventsApplication {
 
 	public static void main(String[] args) {
