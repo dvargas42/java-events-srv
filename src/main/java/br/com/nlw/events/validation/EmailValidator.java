479ce64a8c8c9d63ed9f5c08d4f0a1106a7beb33
dvargas42 <dvargas.eng@gmail.com>
2025-03-04 15:23:52 -0300

    feat: :sparkles: add email validation length

--- src/main/java/br/com/nlw/events/validation/EmailValidator.java
+++ src/main/java/br/com/nlw/events/validation/EmailValidator.java
@@ -6,11 +6,10 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class EmailValidator  implements ConstraintValidator<Email, String> {
+public class EmailValidator implements ConstraintValidator<Email, String> {
 
     private Pattern pattern = Pattern.compile(
-            // "^(?!\\.)(?!.*\\.\\.)[a-zA-Z0-9._%+-]+(?<!\\.)@[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:\\.[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)*\\.[a-zA-Z]{2,}$"
-            "^(?!\\.)(?!.*\\.\\.)[a-zA-Z0-9\\u0080-\\uFFFF._%+-]+(?<!\\.)@[a-zA-Z0-9\\u0080-\\uFFFF](?:[a-zA-Z0-9\\u0080-\\uFFFF-]*[a-zA-Z0-9\\u0080-\\uFFFF])?(?:\\.[a-zA-Z0-9\\u0080-\\uFFFF](?:[a-zA-Z0-9\\u0080-\\uFFFF-]*[a-zA-Z0-9\\u0080-\\uFFFF])?)*\\.[a-zA-Z\\u0080-\\uFFFF]{2,}$" 
+        "^(?!\\.)(?!.*\\.\\.)[a-zA-Z0-9\\u0080-\\uFFFF._%+-]*[a-zA-Z0-9\\u0080-\\uFFFF]@[a-zA-Z0-9\\u0080-\\uFFFF](?:[a-zA-Z0-9\\u0080-\\uFFFF-]{0,61}[a-zA-Z0-9\\u0080-\\uFFFF])?(?:\\.[a-zA-Z0-9\\u0080-\\uFFFF](?:[a-zA-Z0-9\\u0080-\\uFFFF-]{0,61}[a-zA-Z0-9\\u0080-\\uFFFF])?)*\\.[a-zA-Z\\u0080-\\uFFFF]{2,}$"
     );
 
     @Override
@@ -22,6 +21,12 @@ public class EmailValidator  implements ConstraintValidator<Email, String> {
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || value.isEmpty()) {
             return true;
+        }
+
+        String[] emailParts = value.split("@");
+
+        if (emailParts.length != 2 || emailParts[0].length() >= 100 || emailParts[1].length() >= 120) {
+            return false;
         }
 
         Matcher matcher = pattern.matcher(value);
