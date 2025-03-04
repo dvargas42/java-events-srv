7df4265268660636bc096aa1d93c19baad1e2a3b
dvargas42 <dvargas.eng@gmail.com>
2025-03-04 15:27:56 -0300

    feat: :sparkles: change  NumberFormatException to TimeFormatException

--- src/main/java/br/com/nlw/events/validation/TimeValidator.java
+++ src/main/java/br/com/nlw/events/validation/TimeValidator.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+import br.com.nlw.events.exception.TimeFormatException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -38,21 +39,21 @@ public class TimeValidator extends JsonDeserializer<LocalTime> implements Contex
     public LocalTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         try {
             String value = p.getText();
-            if (value == null || value.isEmpty()) {
+            if (value == null || "".equals(value)) {
                 return null;
             }
 
             if (pattern.length() >= 2 && Integer.parseInt(value.substring(0,2)) > 23) {
-                throw new NumberFormatException("Invalid value for hour (valid values 00 - 23)");
+                throw new TimeFormatException("Invalid value for hour (valid values 00 - 23)");
             }
             if (pattern.length() > 3 && Integer.parseInt(value.substring(3,5)) > 59) {
-                throw new NumberFormatException("Invalid value for minutes (valid values 00 - 59)");
+                throw new TimeFormatException("Invalid value for minutes (valid values 00 - 59)");
             }
             if (pattern.length() > 6 && Integer.parseInt(value.substring(6,8)) > 59) {
-                throw new NumberFormatException("Invalid value for seconds (valid values 00 - 59)");
+                throw new TimeFormatException("Invalid value for seconds (valid values 00 - 59)");
             }
             return LocalTime.parse(value, DateTimeFormatter.ofPattern(this.pattern));
-        } catch (NumberFormatException ex) {
+        } catch (TimeFormatException ex) {
             throw JsonMappingException.from(p, ex.getMessage());
         } catch (Exception ex) {
             throw JsonMappingException.from(p, this.message);
