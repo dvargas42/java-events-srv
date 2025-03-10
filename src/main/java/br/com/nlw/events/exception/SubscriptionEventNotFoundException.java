085c30fde743e838de9cc548dffd414ec88b7712
dvargas42 <dvargas.eng@gmail.com>
2025-03-10 02:37:53 -0300

    feat: :sparkles: create SubscriptionEventNotFoundException

---
+++ src/main/java/br/com/nlw/events/exception/SubscriptionEventNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.nlw.events.exception;
+
+public class SubscriptionEventNotFoundException extends RuntimeException {
+
+  public SubscriptionEventNotFoundException(String message) {
+    super(message);
+  } 
+}
