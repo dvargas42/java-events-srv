417607a14bfa3665a5d99f714807126b76d15788
dvargas42 <dvargas.eng@gmail.com>
2025-03-10 20:35:14 -0300

    feat: :sparkles: create RankingEventNotFoundException

---
+++ src/main/java/br/com/nlw/events/exception/RankingEventNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.nlw.events.exception;
+
+public class RankingEventNotFoundException extends RuntimeException {
+
+  public RankingEventNotFoundException(String message) {
+    super(message);
+  }
+}
