11c24223db7718a1be5d910b9b1ace709fc9f0d5
dvargas42 <dvargas.eng@gmail.com>
2025-03-10 02:36:21 -0300

    feat: :goal_net: remove error handling of createSubscription method

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -1,15 +1,7 @@
 package br.com.nlw.events.controller;
 
-import br.com.nlw.events.dto.ErrorMessage;
-import br.com.nlw.events.dto.SubscriptionOut;
-import br.com.nlw.events.dto.SubscriptionRankingItem;
-import br.com.nlw.events.dto.UserIn;
-import br.com.nlw.events.exception.EventNotFoundException;
-import br.com.nlw.events.exception.SubscriptionConflictException;
-import br.com.nlw.events.exception.UserIndicatorNotFoundException;
-import br.com.nlw.events.service.SubscriptionService;
-import jakarta.validation.Valid;
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.nlw.events.dto.ErrorMessage;
+import br.com.nlw.events.dto.SubscriptionRankingItem;
+import br.com.nlw.events.dto.UserIn;
+import br.com.nlw.events.exception.EventNotFoundException;
+import br.com.nlw.events.exception.UserIndicatorNotFoundException;
+import br.com.nlw.events.service.SubscriptionService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/subscription")
@@ -33,17 +33,7 @@ public class SubscriptionController implements ISubscriptionController {
       @PathVariable String prettyName,
       @PathVariable(required = false) Integer userId,
       @Valid @RequestBody UserIn subscriber) {
-    try {
-      SubscriptionOut res = service.createSubscription(prettyName, userId, subscriber);
-      if (!res.equals(null)) {
-        return ResponseEntity.ok(res);
-      }
-    } catch (EventNotFoundException | UserIndicatorNotFoundException ex) {
-      return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
-    } catch (SubscriptionConflictException ex) {
-      return ResponseEntity.status(409).body(new ErrorMessage(ex.getMessage()));
-    }
-    return ResponseEntity.badRequest().build();
+    return ResponseEntity.ok(service.createSubscription(prettyName, userId, subscriber));
   }
 
   @GetMapping("/{prettyName}/ranking")
