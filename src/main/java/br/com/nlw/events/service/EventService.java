352a6a8f627561c176b7feec13e5ed57d24ce6bc
dvargas42 <dvargas.eng@gmail.com>
2025-03-10 01:22:07 -0300

    feat: :sparkles: add not found condition

--- src/main/java/br/com/nlw/events/service/EventService.java
+++ src/main/java/br/com/nlw/events/service/EventService.java
@@ -1,13 +1,16 @@
 package br.com.nlw.events.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import br.com.nlw.events.dto.EventIn;
 import br.com.nlw.events.dto.EventOut;
 import br.com.nlw.events.exception.EventConflictException;
+import br.com.nlw.events.exception.EventNotFoundException;
 import br.com.nlw.events.mapper.IEventMapper;
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.repository.EventRepo;
-import java.util.List;
-import org.springframework.stereotype.Service;
 
 @Service
 public class EventService {
@@ -24,7 +27,8 @@ public class EventService {
     String prettyName = eventIn.title().toLowerCase().replace(" ", "-");
     Event foundEvent = eventRepo.findByPrettyName(prettyName);
     if (foundEvent != null) {
-      throw new EventConflictException("There is already a registration for the event " + foundEvent.getTitle());
+      throw new EventConflictException(
+          "There is already a registration for the event " + foundEvent.getTitle());
     }
     Event event = eventMapper.toEntity(eventIn);
     event.setPrettyName(prettyName);
@@ -36,6 +40,10 @@ public class EventService {
   }
 
   public EventOut getByPrettyName(String prettyName) {
-    return eventMapper.toEventOut(eventRepo.findByPrettyName(prettyName));
+    Event event = eventRepo.findByPrettyName(prettyName);
+    if (event == null) {
+      throw new EventNotFoundException("Event not found with pretty name " + prettyName);
+    }
+    return eventMapper.toEventOut(event);
   }
 }
